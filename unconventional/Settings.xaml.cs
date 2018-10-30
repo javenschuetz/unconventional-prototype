a367fbdf04e02c268e3011a0b56925a14ea1213f
quanaphan <qaphan@ucalgary.ca>
2018-10-30 01:43:16 -0600

    Mocked sample objects

--- unconventional/Settings.xaml.cs
+++ unconventional/Settings.xaml.cs
@@ -24,5 +24,10 @@ namespace unconventional
         {
             InitializeComponent();
         }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
